4c8d70cbbc37e3f65d5bb94692a6856e6c144ca8
Ed Mosqueda (@bbaaxx) <bbaaxx@gmail.com>
2023-01-22 18:06:13 -0600

    bare minimum install

--- setup.sh
+++ setup.sh
@@ -12,22 +12,22 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 sudo apt-get install git-lfs
 sudo -u ubuntu git lfs install --skip-smudge
 
-# download the SD model v2.1 and move it to the SD model directory
-sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
-cd stable-diffusion-2-1-base/
-sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
-sudo -u ubuntu git lfs install --force
-cd ..
-mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
-rm -rf stable-diffusion-2-1-base/
+# # download the SD model v2.1 and move it to the SD model directory
+# sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
+# cd stable-diffusion-2-1-base/
+# sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
+# sudo -u ubuntu git lfs install --force
+# cd ..
+# mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
+# rm -rf stable-diffusion-2-1-base/
 
-# download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
-wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
-cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
+# # download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
+# wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
+# cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
 
-# change ownership of the web UI so that a regular user can start the server
-sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
+# # change ownership of the web UI so that a regular user can start the server
+# sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
 
-# start the server as user 'ubuntu'
+# # start the server as user 'ubuntu'
 # sudo -u ubuntu nohup bash stable-diffusion-webui/webui.sh --listen > log.txt
 
